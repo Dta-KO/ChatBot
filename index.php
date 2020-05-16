7984ba94aefed95f2ee0537e0c8d94a62fabb50d
Dta-KO <55838130+Dta-KO@users.noreply.github.com>
2020-05-16 19:17:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
-                <a class="navbar-brand" href="index.html">UET CHATBOT</a>
+                <a class="navbar-brand" href="index.html">HVHC CHATBOT</a>
             </div>
             
         </div>
@@ -99,7 +99,7 @@
                     <img src="images/user.jpg" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
-                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">UET ChatBot</div>
+                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">HVHC ChatBot</div>
                     <div class="btn-group user-helper-dropdown">
                         <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
                         <ul class="dropdown-menu pull-right">
